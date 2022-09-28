b92dc50e909cc85768a46dc579ee63b06c7fe367
Varsha <varsha.dhama@tokopedia.com>
2022-09-28 12:32:45 +0530

    Add load-more-products functionality to the products list

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from "react";
 import Product from "./Product";
+import classes from './ProductList.module.css';
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
+  const [pageNum, setPageNum] = useState(1);
+  
   useEffect(() => {
-    fetch("https://node-sample-api.herokuapp.com/api/products?page=1")
+    fetch(`https://node-sample-api.herokuapp.com/api/products?page=${pageNum}`)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.data);
-        setProducts(data.data);
+        setProducts((ps) => [...ps, ...data.data]);
       });
-  }, []);
+  }, [pageNum]);
 
+  const loadMoreProducts = () => {
+    setPageNum(ps => ps+1);
+  }
 
   return (
-    <div>
+    <div className={classes.productList}>
         <h2>Products</h2>
       {products.map((product) => (
         <Product src={product.img} name={product.name} price={product.price} />
       ))}
-      <button>Load More</button>
+      <button className={classes.btn} onClick={loadMoreProducts}>Load More</button>
     </div>
   );
 };
