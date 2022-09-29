ae322331a77a90f8c4ac361bb057a0918e569a16
Varsha <varsha.dhama@tokopedia.com>
2022-09-29 07:03:47 +0530

    Add react-router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./components/HomePage";
+import ProductDescriptionPage from "./components/ProductDescriptionPage";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-
-library.add(fas);
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
-      <HomePage />
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/pdp/:productId">
+          <ProductDescriptionPage />
+        </Route>
+      </Switch>
       <Footer />
     </>
   );
