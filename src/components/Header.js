06377efd1ae49982b64fd6674f6881bf3e357945
Varsha <varsha.dhama@tokopedia.com>
2022-09-30 11:35:40 +0530

    Add Link component instead of anchor tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classes from "./Header.module.css";
-
+import { Route, Link } from "react-router-dom";
 import { FaSearch, FaShoppingCart } from "react-icons/fa";
 
 function Header(props) {
@@ -8,18 +8,22 @@ function Header(props) {
     <>
       <header className={classes.header}>
         <div className={classes.brandName}>
-          <a href="/">
+          <Link to="/">
             <h1>T-Mart</h1>
-          </a>
+          </Link>
         </div>
 
         <div className={classes.headerIcons}>
-          <span>
-            <FaShoppingCart />
-          </span>
-          <span>
-            <FaSearch />
-          </span>
+          <Link to="/">
+            <span>
+              <FaShoppingCart />
+            </span>
+          </Link>
+          <Link to="/">
+            <span>
+              <FaSearch />
+            </span>
+          </Link>
         </div>
       </header>
     </>
